0691af60793bbfde607c78e6d1047c5e4d9e2c35
Guest User <Guest@Mac110-Rakdos.local>
2013-02-25 09:31:29 -0500

    All tasks complete.
    I originally committed each task individually, but I had to switch computers due to technical errors, and am currently too tired to work through this again.

---
+++ immutableFunction.h
@@ -0,0 +1,27 @@
+//
+//  immutableFunction.h
+//  
+//
+//  Created by Guest User on 2/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface immutableFunction : NSObject
+@property (readonly) numerator;
+@property (readonly) denominator;
+@property -initWithNumerator: andDenominator: int num int denom;
+@property -initWithFraction: fraction*;
+
+-floatValue (float) someFloat;
+-negative Fraction*;
+-reciprocal Fraction*;
+-sumWith Fraction*;
+-subtractFrom Fraction*;
+-minus Fraction*;
+-multiplyBy Fraction*;
+-divideBy Fraction*;
+-DivideInto Fraction*;
+
+@end
---
+++ immutableFunction.m
@@ -0,0 +1,63 @@
+//
+//  immutableFunction.m
+//  
+//
+//  Created by Guest User on 2/25/13.
+//
+//
+
+#import "immutableFunction.h"
+
+@implementation immutableFunction
+
+-(id) init
+{
+    return Nil;
+}
+
+-(void) description
+{
+    
+}
+
+-negative Fraction*
+{
+    return Nil;
+}
+
+-reciprocal Fraction*
+{
+    return Nil;
+}
+
+-sumWith Fraction*
+{
+    return Nil;
+}
+
+-subtractFrom Fraction*
+{
+    return Nil;
+}
+
+-minus Fraction*
+{
+    return Nil;
+}
+
+-multiplyBy Fraction*
+{
+    return Nil;
+}
+
+-divideBy Fraction*
+{
+    return Nil;
+}
+
+-DivideInto Fraction*
+{
+    return Nil;
+}
+
+@end
