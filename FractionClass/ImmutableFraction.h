b24c3af28ec70b248245646fd8320c50102017d2
Colby Schrom <cschrom@Mac110-Orzhov.local>
2013-03-07 16:11:24 -0500

    P2a setup

---
+++ FractionClass/ImmutableFraction.h
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.h
+//  
+//
+//  Created by Colby Schrom on 3/7/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImmutableFraction : NSObject
+
+@end
---
+++ FractionClass/ImmutableFraction.m
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.m
+//  
+//
+//  Created by Colby Schrom on 3/7/13.
+//
+//
+
+#import "ImmutableFraction.h"
+
+@implementation ImmutableFraction
+
+@end
