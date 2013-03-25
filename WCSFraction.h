3884871d14d8801fc827e17e3de23d04303fff27
Colby Schrom <cschrom@Mac110-Orzhov.local>
2013-03-25 10:35:09 -0400

    Task 3 complete
    Created Class WCSFraction

---
+++ WCSFraction.h
@@ -0,0 +1,13 @@
+//
+//  WCSFraction.h
+//  Fraction Development
+//
+//  Created by Colby Schrom on 3/25/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCSFraction : NSObject
+
+@end
---
+++ WCSFraction.m
@@ -0,0 +1,13 @@
+//
+//  WCSFraction.m
+//  Fraction Development
+//
+//  Created by Colby Schrom on 3/25/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
+//
+
+#import "WCSFraction.h"
+
+@implementation WCSFraction
+
+@end
