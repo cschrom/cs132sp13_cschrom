3483ddd46081c9a2ddc76fcf04ba82d100e1cfde
Colby Schrom <cschrom@Mac110-Orzhov.local>
2013-04-01 18:01:34 -0400

    Task four complete
    Copied over code

--- Fraction Development/WCSAppDelegate.h
+++ Fraction Development/WCSAppDelegate.h
@@ -8,8 +8,28 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface WCSAppDelegate : NSObject <NSApplicationDelegate>
+@interface WCSFraction : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (readonly) int numerator;
+@property (readonly) int denominator;
+@property int numCalculated;
+@property int denomCalculated;
 
+-(id) initWithNumerator:(int)num
+         andDenominator:(int) denom;
+-(id) initWithFraction: FractPtr;
+int GCD(int x, int y);
+
+-(Fract)convertToFract;
+-(float) someFloat;
+-(WCSFraction*) negative;
+-(WCSFraction*) reciprocal;
+-(WCSFraction*) reduced;
+-(WCSFraction*) addTo;
+-(WCSFraction*) subtractFrom;
+-(WCSFraction*) minus;
+-(WCSFraction*) multiply;
+-(WCSFraction*) divideBy;
+-(WCSFraction*) DivideInto;
 @end
--- Fraction Development/WCSAppDelegate.m
+++ Fraction Development/WCSAppDelegate.m
@@ -8,11 +8,216 @@
 
 #import "WCSAppDelegate.h"
 
-@implementation WCSAppDelegate
+@implementation WCSFraction
+@synthesize numerator = _DNU_numeratator;
+@synthesize denominator = _DNU_dinominatator;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+-(id) initWithNumerator:(int)num
+         andDenominator:(int) denom;
 {
-    // Insert code here to initialize your application
+    _DNU_numeratator = 0;
+    _DNU_dinominatator = 0;
+    _numCalculated = 0;
+    _denomCalculated = 0;
+    
+    return Nil;
 }
+
+-(id) initWithFraction:(id)FractPtr
+{
+    return nil;
+}
+
+-(NSString*) description
+{
+    return nil;
+}
+
+-(float) someFloat;
+{
+    return 0;
+}
+
+-(Fract)convertToFract
+{
+    int result;
+    result = _numCalculated / _denomCalculated;
+    return result;
+}
+
+-(WCSFraction*) negative
+{
+    int resultNum = [self numerator];
+    int resultDenom = [self denominator];
+    WCSFraction* result;
+    
+    result = [[WCSFraction alloc] initWithNumerator:resultNum andDenominator:resultDenom];
+    
+    _numCalculated = resultNum;
+    _denomCalculated = resultDenom;
+    
+    [result convertToFract];
+    return [result reduced];
+}
+
+-(WCSFraction*) reciprocal
+{
+    int resultNum = [self denominator];
+    int resultDenom = [self numerator];
+    WCSFraction* result;
+    
+    result = [[WCSFraction alloc] initWithNumerator:resultNum andDenominator:resultDenom];
+    
+    _numCalculated = resultNum;
+    _denomCalculated = resultDenom;
+    
+    [result convertToFract];
+    return [result reduced];
+}
+
+-(WCSFraction*) reduced
+{
+    int GCD(int x, int y);
+    return nil;
+}
+
+-(WCSFraction*) addTo
+{
+    int a = [self numerator];
+    int b = [self numerator];
+    int c = [self denominator];
+    int d = [self denominator];
+    int resultNum;
+    int resultDenom;
+    WCSFraction* result;
+    
+    resultNum = (a * c) + (d * b);
+    resultDenom = c * d;
+    
+    result = [[WCSFraction alloc] initWithNumerator:resultNum andDenominator:resultDenom];
+    
+    _numCalculated = resultNum;
+    _denomCalculated = resultDenom;
+    
+    [result convertToFract];
+    return [result reduced];
+}
+
+-(WCSFraction*) subtractFrom
+{
+    int a = [self numerator];
+    int b = [self numerator];
+    int c = [self denominator];
+    int d = [self denominator];
+    int resultNum;
+    int resultDenom;
+    WCSFraction* result;
+    
+    resultNum = a * c - b * d;
+    resultDenom = c * d;
+    
+    result = [[WCSFraction alloc] initWithNumerator:resultNum andDenominator:resultDenom];
+    
+    _numCalculated = resultNum;
+    _denomCalculated = resultDenom;
+    
+    [result convertToFract];
+    return [result reduced];
+}
+
+-(WCSFraction*) minus
+{
+    int a = [self numerator];
+    int b = [self numerator];
+    int c = [self denominator];
+    int d = [self denominator];
+    int resultNum;
+    int resultDenom;
+    
+    WCSFraction* result;
+    
+    resultNum = (a * c) - (d * b);
+    resultDenom = c * d;
+    
+    result = [[WCSFraction alloc] initWithNumerator:resultNum andDenominator:resultDenom];
+    
+    _numCalculated = resultNum;
+    _denomCalculated = resultDenom;
+    
+    [result convertToFract];
+    return [result reduced];
+}
+
+-(WCSFraction*) multiply
+{
+    int a = [self numerator];
+    int b = [self numerator];
+    int c = [self denominator];
+    int d = [self denominator];
+    int resultNum;
+    int resultDenom;
+    WCSFraction* result;
+    
+    resultNum = a * b;
+    resultDenom = c * d;
+    
+    result = [[WCSFraction alloc] initWithNumerator:resultNum andDenominator:resultDenom];
+    
+    _numCalculated = resultNum;
+    _denomCalculated = resultDenom;
+    
+    [result convertToFract];
+    return [result reduced];
+}
+
+-(WCSFraction*) divideBy
+{
+    int a = [self numerator];
+    int b = [self numerator];
+    int c = [self denominator];
+    int d = [self denominator];
+    int resultNum;
+    int resultDenom;
+    WCSFraction* result;
+    
+    resultNum = a * d;
+    resultDenom = c * b;
+    
+    result = [[WCSFraction alloc] initWithNumerator:resultNum andDenominator:resultDenom];
+    
+    _numCalculated = resultNum;
+    _denomCalculated = resultDenom;
+    
+    [result convertToFract];
+    return [result reduced];
+}
+
+-(WCSFraction*) DivideInto
+{
+    int a = [self numerator];
+    int b = [self numerator];
+    int c = [self denominator];
+    int d = [self denominator];
+    int resultNum;
+    int resultDenom;
+    WCSFraction* result;
+    
+    resultNum = b * c;
+    resultDenom = d * a;
+    
+    result = [[WCSFraction alloc] initWithNumerator:resultNum andDenominator:resultDenom];
+    
+    _numCalculated = resultNum;
+    _denomCalculated = resultDenom;
+    
+    [result convertToFract];
+    return [result reduced];
+}
+
+int GCD(int x, int y)
+{
+    return 0;
+}
+
 
 @end
