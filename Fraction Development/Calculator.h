ce513e00788255b9f5ffac7d3f0dfc1b8bcbcb0f
Colby Schrom <cschrom@Mac110-Orzhov.local>
2013-05-03 16:47:39 -0400

    Declared WCSFractionPart
    and declared the typedef structure that goes with it.

--- Fraction Development/Calculator.h
+++ Fraction Development/Calculator.h
@@ -1,10 +1,16 @@
 #import <Foundation/Foundation.h>
 
+typedef enum
+{
+    WCSPartNumerator,
+    WCSPartDenominator
+} WCSPartofFraction;
+
 @interface Calculator: NSObject;
 @property (assign) int numberOnScreen;
 @property (assign) int numberAccumulated;
 @property (assign) char operationPending;
-
+@property (assign) WCSPartofFraction WCSFractionPart;
 
 -(void) pressKey: (char) theKey;
 -(void) appendDigit: (char) charDigit;
@@ -13,6 +19,8 @@
 -(void) computeAndDisplayResult;
 -(void) clearAccumulator: (char) accumulatorClearer;
 -(void) clearOperation: (char) operationClearer;
+-(void) switchPart: (char) switcher;
+-(void) deleteSpace: (char) deleteKey;
 
 @end
 
