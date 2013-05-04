d67d2d9154bd3099bcacd2416180cdd3a9275b02
Guest User <Guest@Mac110-Dimir.local>
2013-05-03 23:34:21 -0400

    Again no changes
    I mustv'e missed this while uploading

--- Fraction Calculator/Fraction Calculator/WCSMutableFraction.m
+++ Fraction Calculator/Fraction Calculator/WCSMutableFraction.m
@@ -1,13 +1,75 @@
 //
 //  WCSMutableFraction.m
-//  Fraction Calculator
+//  Fraction Development
 //
-//  Created by Guest User on 5/3/13.
-//  Copyright (c) 2013 Guest User. All rights reserved.
+//  Created by Colby Schrom on 4/18/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
 //
 
 #import "WCSMutableFraction.h"
 
 @implementation WCSMutableFraction
+@synthesize numerator = _AOII_numerator;
+@synthesize denominator = _AOII_denominator;
 
+-(void) setNumerator:(int)num
+      andDenominator:(int)denom
+{
+    int GCD = gcd(num,denom);
+    
+    [self setNumerator:num/GCD];
+    [self setDenominator:denom/GCD];
+}
+
+-(id)mutablecopyWithZone:(NSZone*)zone
+{
+    WCSFraction* theFraction;
+    theFraction = [[WCSFraction allocWithZone:zone] initWithFraction:self];
+    return theFraction;
+}
+
+-(void) modifyByAdding:(WCSFraction*)fract
+{
+    int a = [self numerator];
+    int b = [fract numerator];
+    int c = [self denominator];
+    int d = [fract denominator];
+    int resultNum;
+    int resultDenom;
+    
+    resultNum = (a * d) + (c * b);
+    resultDenom = c * d;
+    
+    [self setNumerator:resultNum
+        andDenominator:resultDenom];
+    
+}
+
+-(void)modifyByMultiplying:(WCSFraction*)fract
+{
+    int a = [self numerator];
+    int b = [fract numerator];
+    int c = [self denominator];
+    int d = [fract denominator];
+    int resultNum;
+    int resultDenom;
+    
+    resultNum = a * b;
+    resultDenom = c * d;
+    
+    [self setNumerator:resultNum
+        andDenominator:resultDenom];
+}
+
+-(void)modifyByInverting
+{
+    [self reciprocal];
+    
+}
+
+-(void)modifyByNegating
+{
+    [self negative];
+}
 @end
+
